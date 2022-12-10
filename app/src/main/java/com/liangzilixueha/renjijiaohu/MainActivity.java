c3045fae2ae190fc61b5071887fc0fac49aa4bfb
liangzilixueha <542876162@qq.com>
2022-12-10 21:10:56 +0800

    主界面√

--- app/src/main/java/com/liangzilixueha/renjijiaohu/MainActivity.java
+++ app/src/main/java/com/liangzilixueha/renjijiaohu/MainActivity.java
@@ -3,12 +3,37 @@ package com.liangzilixueha.renjijiaohu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.liangzilixueha.renjijiaohu.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        //取消标题栏
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+        binding.tab.setOnClickListener(v -> {
+            binding.tvGuidance.getVisibility();
+            binding.tvLocation.getVisibility();
+            binding.tvPersonal.getVisibility();
+            //转换能见性
+            if (binding.tvGuidance.getVisibility() == View.VISIBLE) {
+                binding.tvGuidance.setVisibility(View.GONE);
+                binding.tvLocation.setVisibility(View.GONE);
+                binding.tvPersonal.setVisibility(View.GONE);
+            } else {
+                binding.tvGuidance.setVisibility(View.VISIBLE);
+                binding.tvLocation.setVisibility(View.VISIBLE);
+                binding.tvPersonal.setVisibility(View.VISIBLE);
+            }
+        });
     }
 }
